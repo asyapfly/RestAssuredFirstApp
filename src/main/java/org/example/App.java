254d94d0e05a858c20d4386f1823fdcdd11f239d
IV <arsenybur@gmail.com>
2020-10-06 10:26:11 +0400

    Попытка использовать RestAssured

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //RestAssured.given() //TODO ИЗУЧИТЬ ФРЕЙМВОРК!
+               // .body()
+    }
+
+
+}
